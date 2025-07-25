6981cf01baba15558dd554581d84456f11248e02
evilboss <jr.evilboss@gmail.com>
2025-07-25 21:26:28 +0800

    add plaid lib

---
+++ src/lib/plaid.js
@@ -0,0 +1,16 @@
+// lib/plaid.js
+import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
+
+const config = new Configuration({
+  basePath: PlaidEnvironments[process.env.PLAID_ENV],
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+      'PLAID-SECRET': process.env.PLAID_SECRET,
+    },
+  },
+});
+
+const plaidClient = new PlaidApi(config);
+
+export default plaidClient;
